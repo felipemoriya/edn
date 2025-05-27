1b4dced083c1ac4e94bb6a1f6309de7b3aa04c2c
Felipe <tackberry@bol.com.br>
2025-05-27 20:37:39 -0300

    Upload Escola da Nuvem

--- Escola da Nuvem - Atividades Práticas/04Ex02-atividade-02.py
+++ Escola da Nuvem - Atividades Práticas/04Ex02-atividade-02.py
@@ -7,27 +7,27 @@
 notas = []
 
 while True:
-        entrada = (input("Digite a nota do aluno (ou 'fim' para encerrar): "))
+    entrada = input("Digite a nota do aluno (ou 'fim' para encerrar): ")
 
-    if entrada.lower() =='fim':
+    if entrada.lower() == 'fim':
         break
-    
+
     try:
         nota = float(entrada)
-
         if 0 <= nota <= 10:
-                notas.append(nota)
+            notas.append(nota)
         else:
             print("Nota inválida. Digite um valor entre 0 e 10.")
     except ValueError:
-       print("Entrada Inválida. Por favor, digite um número ou 'fim'")
+        print("Entrada inválida. Por favor, digite um número ou 'fim'.")
 
 if len(notas) > 0:
-       media = sum(notas) / len(notas)
-       print(f"\nMédia da turma: {media:.2f}")
-       print(f"Total de notas válidas registradas: {len{notas}}")
+    media = sum(notas) / len(notas)
+    print(f"\nMédia da turma: {media:.2f}")
+    print(f"Total de notas válidas registradas: {len(notas)}")
 else:
-        print("\nNenhuma nota válida foi registrada. ")
+    print("\nNenhuma nota válida foi registrada.")
+
 
 
 
